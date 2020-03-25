ef4c5db8d6177995eb6c05c03a3cf9826cf96f84
wil.i.am <wiliam.tae@gmail.com>
2020-03-25 02:36:14 -0300

    backend database setup ended

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,25 +1,24 @@
 const express = require('express');
 const crypto = require('crypto');
+
 const connection = require('./database/connection');
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
-routes.post('/ongs', async (req, resp) => {
-  const { name, email, whatsapp, city, uf } = req.body;
+routes.post('/sessions', SessionController.create);
 
-  const id = crypto.randomBytes(4).toString('HEX');
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 
-  await connection('ongs').insert({
-    id,
-    name,
-    email,
-    whatsapp,
-    city,
-    uf,
-  });
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
-  return resp.json({ id });
-});
+routes.get('/profile', ProfileController.index);
 
 module.exports = routes;
 
